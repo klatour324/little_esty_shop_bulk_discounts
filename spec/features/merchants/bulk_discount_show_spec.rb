03ea8111b8dfa31b6e956b1baf9621fac09d4772
Katy La Tour <klatour324@gmail.com>
2021-03-07 10:48:30 -0600

    Add feature spec to have Edit Bulk Discount Link on Bulk Discount Show page

--- spec/features/merchants/bulk_discount_show_spec.rb
+++ spec/features/merchants/bulk_discount_show_spec.rb
@@ -20,5 +20,26 @@ RSpec.describe "Merchant Bulk Discount Show" do
         expect(page).to_not have_content(@bulk_discount_2.percent_discount)
       end
     end
+
+    it "can edit the bulk discount information when clicking the edit link" do
+      VCR.use_cassette("bulk_discount_creation") do
+        visit "/merchant/#{@merchant1.id}/bulk_discounts/#{@bulk_discount_1.id}"
+
+        expect(page).to have_link("Edit Bulk Discount")
+
+        click_link("Edit Bulk Discount")
+
+        expect(current_path).to eq("/merchant/#{@merchant1.id}/bulk_discounts/#{@bulk_discount_1.id}/edit")
+      end
+    end
   end
 end
+
+# When I visit my bulk discount show page
+# Then I see a link to edit the bulk discount
+# When I click this link
+# Then I am taken to a new page with a form to edit the discount
+# And I see that the discounts current attributes are pre-poluated in the form
+# When I change any/all of the information and click submit
+# Then I am redirected to the bulk discount's show page
+# And I see that the discount's attributes have been updated
