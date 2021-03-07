5faf49ca91e58f2cfa1bd9205ce9b3b9cdc09b33
Katy La Tour <klatour324@gmail.com>
2021-03-06 18:53:02 -0600

    Create public holidays instance variable in index to get access to next US public holidays in bulk discounts index view

--- app/controllers/bulk_discounts_controller.rb
+++ app/controllers/bulk_discounts_controller.rb
@@ -1,5 +1,6 @@
 class BulkDiscountsController < ApplicationController
   def index
     @merchant = Merchant.find(params[:merchant_id])
+    @public_holidays = NagerDateService.new("US").next_public_holidays
   end
 end
