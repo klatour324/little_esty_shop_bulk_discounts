76087970a91018bdc564fcf0ed79b5b01119d79b
Katy La Tour <klatour324@gmail.com>
2021-03-07 14:59:23 -0600

    Add spec to check for invalid data and render a flash error message to screen if any information is incorrect

--- spec/features/merchants/bulk_discount_edit_spec.rb
+++ spec/features/merchants/bulk_discount_edit_spec.rb
@@ -36,5 +36,21 @@ RSpec.describe "Merchant Bulk Discount Edit Page" do
         expect(page).to have_content("Percent Discount: 0.1")
       end
     end
+
+    it "cannot edit a form with invalid input on form" do
+      VCR.use_cassette("bulk_discount_creation") do
+        visit "/merchant/#{@merchant1.id}/bulk_discounts/#{@bulk_discount_1.id}/edit"
+
+        fill_in("bulk_discount[name]", with: "")
+        fill_in("bulk_discount[item_threshold]", with: 100)
+        fill_in("bulk_discount[percent_discount]", with: 0.0)
+
+        click_button("Update Bulk Discount")
+
+        expect(page).to have_content("Bulk Discount has not been Updated. Confirm all fields are filled in with correct information. Item Threshold must be greater than 1 and Percent Discount must be greater than 0 and less than 1.")
+        expect(current_path).to eq("/merchant/#{@merchant1.id}/bulk_discounts/#{@bulk_discount_1.id}/edit")
+        expect(page).to have_button("Update Bulk Discount")
+      end
+    end
   end
 end
