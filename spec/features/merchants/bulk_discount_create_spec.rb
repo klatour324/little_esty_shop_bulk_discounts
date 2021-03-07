4fde9a94397b173b30427312b6d072e92c97b528
Katy La Tour <klatour324@gmail.com>
2021-03-06 23:02:44 -0600

    Add bulk discount creation story where user can click link and fill out a form to add a new bulk discount

--- spec/features/merchants/bulk_discount_create_spec.rb
+++ spec/features/merchants/bulk_discount_create_spec.rb
@@ -3,32 +3,29 @@ require "rails_helper"
 RSpec.describe "Merchant Bulk Discount Create Page" do
   before :each do
     @merchant = Merchant.create!(name: 'Pawtrait Designs')
-    # @bulk_discount_1 = BulkDiscount.create!(item_threshold: 5, percent_discount: 0.05, name:'Senior Day Discount', merchant_id: @merchant1.id)
-    # @bulk_discount_2 = BulkDiscount.create!(item_threshold: 10, percent_discount: 0.10, name:'Christmas Discount', merchant_id: @merchant1.id)
-    # @bulk_discount_3 = BulkDiscount.create!(item_threshold: 15, percent_discount: 0.15, name:' 4th of July Discount', merchant_id: @merchant1.id)
-    # @bulk_discount_4 = BulkDiscount.create!(item_threshold: 20, percent_discount: 0.20, name:'Halloween Discount', merchant_id: @merchant1.id)
   end
 
-  describe "When I visit my bulk discounts index" do
-    it "shows a link to create a new discount" do
-      VCR.use_cassette("nager_data_service_next_public_holidays") do
-        visit merchant_bulk_discounts_path(@merchant)
+  describe "Bulk discount creation" do
+    it "user can create a new discount" do
+      VCR.use_cassette("bulk_discount_creation") do
+        visit "/merchant/#{@merchant.id}/bulk_discounts"
 
         expect(page).to have_link("Create New Discount")
 
         click_link("Create New Discount")
 
-        expect(current_path).to eq(new_merchant_bulk_discount_path(@merchant))
+        expect(current_path).to eq("/merchant/#{@merchant.id}/bulk_discounts/new")
+        expect(page).to have_content("Create a New Bulk Discount")
+
+        fill_in "name", with: "End of Bin Deal!"
+        fill_in "item_threshold", with: 5
+        fill_in "percent_discount", with: 0.10
+
+        click_button("Add New Bulk Discount")
+
+        expect(current_path).to eq("/merchant/#{@merchant.id}/bulk_discounts")
+        expect(page).to have_content("End of Bin Deal!")
       end
     end
   end
 end
-
-# As a merchant
-# When I visit my bulk discounts index
-# Then I see a link to create a new discount
-# When I click this link
-# Then I am taken to a new page where I see a form to add a new bulk discount
-# When I fill in the form with valid data
-# Then I am redirected back to the bulk discount index
-# And I see my new bulk discount listed
