b10494634edc4a351e6fd63219803b5b7499b62e
Katy La Tour <klatour324@gmail.com>
2021-03-06 19:46:13 -0600

    Add new action to bulk discounts controller

--- app/controllers/bulk_discounts_controller.rb
+++ app/controllers/bulk_discounts_controller.rb
@@ -3,4 +3,7 @@ class BulkDiscountsController < ApplicationController
     @merchant = Merchant.find(params[:merchant_id])
     @public_holidays = NagerDateService.new("US").next_public_holidays
   end
+
+  def new
+  end
 end
