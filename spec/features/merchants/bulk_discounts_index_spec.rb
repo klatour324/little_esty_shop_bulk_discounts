a7f0d8519685ffcf32b6d2efe83f3564be3bc725
Katy La Tour <klatour324@gmail.com>
2021-03-06 18:54:37 -0600

    Remove commented out lines and clean up file

--- spec/features/merchants/bulk_discounts_index_spec.rb
+++ spec/features/merchants/bulk_discounts_index_spec.rb
@@ -68,10 +68,3 @@ RSpec.describe "Merchant Bulk Discounts Index" do
     end
   end
 end
-
-# As a merchant
-# When I visit the discounts index page
-# I see a section with a header of "Upcoming Holidays"
-# In this section the name and date of the next 3 upcoming US holidays are listed.
-#
-# Use the Next Public Holidays Endpoint in the [Nager.Date API](https://date.nager.at/swagger/index.html)
