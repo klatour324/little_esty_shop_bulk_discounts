b8a3935f5c9690ab9efeea4df977140206fad823
Katy La Tour <klatour324@gmail.com>
2021-03-08 02:07:59 -0600

    Add total revenue method to invoice model to add up all the invoice item revenues

--- app/models/invoice.rb
+++ app/models/invoice.rb
@@ -11,6 +11,8 @@ class Invoice < ApplicationRecord
   enum status: [:cancelled, :in_progress, :complete]
 
   def total_revenue
-    invoice_items.sum("unit_price * quantity")
+    invoice_items.sum do |invoice_item|
+      invoice_item.revenue
+    end
   end
 end
