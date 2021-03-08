e368bfefa3863e6ec3118945dd650fe4252814d3
Katy La Tour <klatour324@gmail.com>
2021-03-08 01:58:58 -0600

    Actually implement the available discount and revenue method here

--- app/models/invoice_item.rb
+++ app/models/invoice_item.rb
@@ -16,4 +16,18 @@ class InvoiceItem < ApplicationRecord
     invoice_ids = InvoiceItem.where("status = 0 OR status = 1").pluck(:invoice_id)
     Invoice.order(created_at: :asc).find(invoice_ids)
   end
+
+  def available_discount
+    item
+    .merchant
+    .bulk_discounts
+    .order(percent_discount: :desc)
+    .where('item_threshold <= ?', "#{self.quantity}")
+    .first
+  end
+
+  def revenue
+    return unit_price * quantity if available_discount.nil?
+    unit_price * quantity * (1 - available_discount.percent_discount)
+  end
 end
