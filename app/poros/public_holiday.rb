7efdb356cd788157f1d7a953fe94b5a93accadbc
Katy La Tour <klatour324@gmail.com>
2021-03-09 14:23:21 -0600

    Add date to poros initialize method

--- app/poros/public_holiday.rb
+++ app/poros/public_holiday.rb
@@ -1,7 +1,9 @@
 class PublicHoliday
-  attr_reader :local_name
+  attr_reader :local_name,
+              :date
 
   def initialize(api_data)
     @local_name = api_data[:localName]
+    @date = api_data[:date]
   end
 end
